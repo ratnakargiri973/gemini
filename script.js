86c1c91d97d3794e096277b8277d1abc8baf4d60
Ratnakar <ratnakargiri973@gmail.com>
2024-06-07 21:42:21 +0530

    files added

---
+++ script.js
@@ -0,0 +1,32 @@
+// let API_KEY="AIzaSyDyMKDU9TVHpgxM_LPlWURPSQD_85JhtBA";
+
+
+
+    //   import { GoogleGenerativeAI } from "@google/generative-ai";
+
+    
+    //   const API_KEY = "AIzaSyDyMKDU9TVHpgxM_LPlWURPSQD_85JhtBA";
+
+    //   const genAI = new GoogleGenerativeAI(API_KEY);
+    //   let btn = document.querySelector("#btn");
+    //   let displayContainer = document.getElementById("display-container")
+
+    //  async function run(){
+    //   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+    //   let user_input = document.querySelector("#searchBox").value;
+    //   console.log(user_input);
+    //   const prompt = user_input;
+
+
+    //   const result = await model.generateContent(prompt);
+    //   const response = await result.response;
+    //   const text = response.text();
+    //   console.log(text);
+
+    //   let botmessagediv = document.createElement("div");
+    //       botmessagediv.innerText = text;
+    //       displayContainer.appendChild(botmessagediv)
+
+    //  }
+    //  btn.addEventListener("click", run);
+    console.log("hello");
